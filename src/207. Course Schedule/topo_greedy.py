86e7f4b136f79c284e4caaa54077e538b02a5842
yutong-xie <36655655+yutong-xie@users.noreply.github.com>
2020-08-24 23:11:05 -0500

    update 207

---
+++ src/207. Course Schedule/topo_greedy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+    Copyright 2020, Yutong Xie, UIUC.
+    Using greedy algorithm verison topological sort to solve the problem
+ '''
+def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+
+        indegree = [0]*numCourses
+        graph = [[] for i in range(numCourses)]
+        stack = []
+        result = []
+
+        for y,x in prerequisites:
+            indegree[y] += 1
+            graph[x].append(y)
+
+
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                stack.append(i)
+
+        while len(stack) != 0:
+            v = stack.pop()
+            result.append(v)
+            for i in graph[v]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    stack.append(i)
+
+        return True if len(result) == numCourses else False numCourses else False
